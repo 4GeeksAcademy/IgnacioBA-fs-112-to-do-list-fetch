a224228fa6e3842224645202c5dbb4ef4d2d4f61
IgnacioBA9 <150173378+IgnacioBA9@users.noreply.github.com>
2025-06-20 11:15:18 +0000

    [fix:]getTAskList function minor fix

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 
 //create your first component
@@ -6,33 +6,38 @@ import React, {useState, useEffect} from "react";
 
 const Home = () => {
 
-	const [taskList, setTaskList]=useState([]);
+	const [taskList, setTaskList] = useState([]);
 
 	function getTaskList() {
-		fetch('https://playground.4geeks.com/todo/users/igna',{method:"GET"})// buscar informacion en la url
-		.then((response)=>{
-			return response.json()}) // si llega una respuesta prometo que la convierto en un formato utilizable JSON
-		.then((data)=>setTaskList(data.todos))
-		//.then((data)=>setCharacters(data.results)) // Prometo que si el formato a json sale bien lo guardo en un espacio
-		.catch((error)=>console.log(error)) // si algo sale, lo aviso
+		fetch('https://playground.4geeks.com/todo/users/igna', { method: "GET" })// buscar informacion en la url
+			.then((response) => {
+				return response.json()
+			}) // si llega una respuesta prometo que la convierto en un formato utilizable JSON
+			.then((data) => setTaskList(data.todos))
+			//.then((data)=>setCharacters(data.results)) // Prometo que si el formato a json sale bien lo guardo en un espacio
+			.catch((error) => console.log(error)) // si algo sale, lo aviso
 	}
 
-	
 
-	useEffect(()=>{
+
+	useEffect(() => {
 		//codigo que queremos que se ejecute cuando se cargue el componente
-		{taskList.map((item, index) => (
-                    <li key={index}>{item.label}</li>
-                ))}
 		getTaskList()
-	},[])
+	}, [])
 	console.log(taskList);
 
 	return (
 		<div className="text-center">
-            
-		
-			
+
+			<div className="container">
+				<h2>Mis tareas</h2>
+				<ul>
+					{taskList.map((item, index) => (
+						<li key={index}>{item.label}</li>
+					))}
+				</ul>
+			</div>
+
 		</div>
 	);
 };
